80790b669c380f3ac970315585b958cd4ce1a50f
Afif <180865K@mymail.nyp.edu.sg>
2021-04-19 10:28:13 +0800

    Created project

---
+++ LedBottomToggleTask/LedBottomToggleTask/StartupTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+using System.Threading;
+using System.Threading.Tasks;
+using GrovePi;
+using GrovePi.Sensors;
+
+
+namespace LedBottomToggleTask
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // 
+            // TODO: Insert code to perform background work
+            //
+            // If you start any asynchronous methods here, prevent the task
+            // from closing prematurely by using BackgroundTaskDeferral as
+            // described in http://aka.ms/backgroundtaskdeferral
+            //
+        }
+    }
+}
